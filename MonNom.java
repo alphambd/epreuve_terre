9915636782b10d099d6218ee07b9c37ebdd0d91e
Alpha Diallo <alphadiallo.mb@gmail.com>
2022-11-01 14:27:19 +0100

    Suppression MonNom.java

--- MonNom.java
+++
@@ -1,6 +0,0 @@
-public class MonNom{
-  public static void main(String[] args){
-    System.out.println("Je n'arrive pas à récupérer le nom de mon fichier <:::> ");
-    System.out.println(MonNom.getSimpleName());
-  }
-}
